4580595df4ad47d784c4d78fb509f3cfd314dae8
Corentin VIVES <c.vives@groupeonepoint.com>
2022-01-03 17:14:10 +0100

    [feat] CustomerRestController: post request: add Customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,12 @@ public class CustomerRestController {
 		}
 		
 		return customer;
+	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer c) {
+		c.setId(0);
+		customerService.saveCustomer(c);
+		return c;
 	}
 }
